f0b42759b85689bb97f2182207314b5f9acc1b83
ekady6 <baeklin02@gmail.com>
2021-02-12 07:17:24 +0900

    output

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>6018</title>
+        <meta name="description" content="Manufacture convenience apps." data-react-helmet="true"/>
+        <meta name="google-site-verification" content="Z5F5-UQiR6XA8okFrQoK4Wqj87dRAlk2MWGfZsmeVuM"/>
+      </Helmet>
       <header className="App-header">
-        <p>
-          ...
-        </p>
+        <p style={{marginLeft: 0.6, marginTop: 0.6, marginRight: 0.6, marginBottom: 1.6, fontSize: 2.8 + 'em'}}>6018</p>
+        <ul>
+          <li>
+            <a href="https://soundcloud.com/wd6018" target="_blank">SoundCloud</a>
+          </li>
+          <li>
+            <a href="https://twitter.com/_ekady6" target="_blank">Twitter</a>
+          </li>
+          <li>
+            <a href="https://github.com/ekady6/" target="_blank">Github</a>
+          </li>
+        </ul>
       </header>
     </div>
   );
